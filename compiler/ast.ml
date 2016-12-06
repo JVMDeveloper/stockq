00414a97fd5829e15250cdb10cda7fa59c67dc69
Ricardo Martinez <rmm783@gmail.com>
2016-12-05 23:33:30 -0500

    initial commit for stockq
    all previous work done in the
    github.com/sophielucy/stockx
    repository

---
+++ compiler/ast.ml
@@ -0,0 +1,42 @@
+(* Abstract Syntax Tree and functions for printing it *)
+
+type op = Add | Sub | Mult | Div | Mod | Equal |
+          Neq | Less | Leq | Greater | Geq | And | Or
+
+type uop = Neg | Not
+
+type typ = Int | Float | Bool | Void | Stock | Order |
+           Portfolio | String | Array | Struct
+
+type bind = typ * string
+
+type expr =
+    IntLiteral of int
+  | FloatLiteral of float
+  | StringLiteral of string
+  | BoolLiteral of bool
+  | Id of string
+  | Binop of expr * op * expr
+  | Unop of uop * expr
+  | Assign of string * expr
+  | Call of string * expr list
+  | ObjAccess of expr * expr
+  | Noexpr
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | While of expr * stmt
+  | Local of typ * string * expr
+
+type func_decl = {
+    fname : string;
+    formals : bind list;
+	typ : typ;
+    body : stmt list;
+  }
+
+type program = func_decl list * stmt list
