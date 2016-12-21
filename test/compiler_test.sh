96a85427d6623756fae08656333527fa2c826ec9
Ricardo Martinez <rmm783@gmail.com>
2016-12-20 21:41:29 -0500

    error testing

--- test/compiler_test.sh
+++ test/compiler_test.sh
@@ -6,6 +6,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 
 INPUT_FILES="compiler/*.sq"
+ERROR_FILES="compiler/fail-*.sq"
 printf "${CYAN}Running compiler tests...\n${NC}"
 
 for input_file in $INPUT_FILES; do
@@ -18,6 +19,27 @@ for input_file in $INPUT_FILES; do
 
     lli "$llfile" > "$tmpfile"
     echo '' >> "$tmpfile"
+
+    # if test output file exists, compare compiled output to it
+    if [ -e "$output_file" ]; then
+        cmp -s $tmpfile $output_file
+        if [ "$?" -ne 0 ]; then
+            printf "%-65s ${RED}ERROR\n${NC}" "  - checking $output_file..." 1>&2
+            rm -f $llfile $tmpfile
+            exit 1
+        fi
+    fi
+
+    printf "%-65s ${GREEN}SUCCESS\n${NC}" "  - checking $input_file..."
+    rm -f $llfile $tmpfile
+done
+
+for input_file in $ERROR_FILES; do
+    tmpfile=${input_file/.err/.tempout}
+    output_file=${input_file/.err/.out}
+
+    # compile stockq program to llvm file
+    ../compiler/stockq -c &> $tmpfile < $input_file
 
     # if test output file exists, compare compiled output to it
     if [ -e "$output_file" ]; then
